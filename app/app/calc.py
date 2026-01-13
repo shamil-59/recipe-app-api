c4c2e3f5bd2942bbeb73a7b9d7780c94ed6d51b8
shamil-59 <mursalov.sh.a@gmail.com>
2026-01-13 20:48:02 +0100

    Adding postgres.

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator function
+"""
+
+def add(x, y):
+    """Test adding numbers."""
+    return x + y
+
+
+def subtract(x, y):
+    """Test subtracing numbers."""
+    return y - x
+    
